fe1e9cf1164b4b7c3e867ad11608f1e0b9ace065
KienbMi <m_kienberger@gmx.net>
2019-12-06 20:29:11 +0100

    add template

---
+++ template/source/NumberGenerator.Logic/BaseObserver.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace NumberGenerator.Logic
+{
+    /// <summary>
+    /// Beobachter, welcher die Zahlen auf der Konsole ausgibt.
+    /// Diese Klasse dient als Basisklasse für komplexere Beobachter.
+    /// </summary>
+    public class BaseObserver : IObserver
+    {
+        #region Fields
+
+        private readonly IObservable _numberGenerator;
+
+        #endregion
+
+        #region Properties
+
+        public int CountOfNumbersReceived { get; private set; }
+        public int CountOfNumbersToWaitFor { get; private set; }
+
+        #endregion
+
+        #region Constructors
+
+        public BaseObserver(IObservable numberGenerator, int countOfNumbersToWaitFor)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #region Methods
+
+        #region IObserver Members
+
+        /// <summary>
+        /// Wird aufgerufen wenn der NumberGenerator eine neue Zahl generiert hat.
+        /// </summary>
+        /// <param name="number"></param>
+        public virtual void OnNextNumber(int number)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        public override string ToString()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected void DetachFromNumberGenerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
